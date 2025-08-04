9f852b8a13f6efd763a513bb886d8b475f8a3933
Tomas178 <tomaspetronis77@gmail.com>
2025-08-04 21:57:53 +0300

    Add all repositories to be exported

--- server/src/repositories/index.ts
+++ server/src/repositories/index.ts
@@ -1,9 +1,19 @@
 import type { Database } from '@server/database';
 import { recipesRepository } from './recipesRepository';
+import { ingredientsRepository } from './ingredientsRepository';
+import { recipesIngredientsRepository } from './recipesIngredientsRepository';
+import { toolsRepository } from './toolsRepository';
+import { recipesToolsRepository } from './recipesToolsRepository';
 
 export type RepositoryFactory = <T>(database: Database) => T;
 
-const repositories = { recipesRepository };
+const repositories = {
+  recipesRepository,
+  ingredientsRepository,
+  recipesIngredientsRepository,
+  toolsRepository,
+  recipesToolsRepository,
+};
 
 export type RepositoriesFactories = typeof repositories;
 export type Repositories = {
