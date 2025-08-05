02dc226224b2ea8910b8691b90e36e72ba97fe52
Tomas178 <tomaspetronis77@gmail.com>
2025-08-05 19:42:47 +0300

    (Feat): add pagination schema

--- server/src/controllers/recipes/findAll.ts
+++ server/src/controllers/recipes/findAll.ts
@@ -1,9 +1,7 @@
 import { publicProcedure } from '@server/trpc';
 import provideRepos from '@server/trpc/provideRepos';
-import * as z from 'zod';
 import { recipesRepository } from '@server/repositories/recipesRepository';
-
-const POSTGRES_INT_MAX = 2141483647;
+import { paginationSchema } from '@server/entities/shared';
 
 export default publicProcedure
   .use(
@@ -12,15 +10,10 @@ export default publicProcedure
     })
   )
   .input(
-    z
-      .object({
-        offset: z.number().int().min(0).max(POSTGRES_INT_MAX),
-        limit: z.number().int().min(1).max(100),
-      })
-      .default({
-        offset: 0,
-        limit: 5,
-      })
+    paginationSchema.default({
+      offset: 0,
+      limit: 5,
+    })
   )
   .query(async ({ input, ctx: { repos } }) => {
     const recipes = await repos.recipesRepository.findAll(input);
--- server/src/entities/shared.ts
+++ server/src/entities/shared.ts
@@ -9,3 +9,10 @@ export const ingredientToolNameSchema = z.string().nonempty();
 export const createdAtSchema = z.date().default(() => new Date());
 export const updatedAtSchema = z.date().default(() => new Date());
 export const expiresAtSchema = z.date().default(() => new Date());
+
+const POSTGRES_INT_MAX = 2141483647;
+
+export const paginationSchema = z.object({
+  offset: z.number().int().min(0).max(POSTGRES_INT_MAX),
+  limit: z.number().int().min(1).max(100),
+});
