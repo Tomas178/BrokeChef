c802cfe786e2dc78c2b38997d1cbbaef4aa341ac
Tomas178 <tomaspetronis77@gmail.com>
2025-08-07 12:45:57 +0300

    (Feat): add procedure only for recipe author

---
+++ server/src/trpc/recipeAuthorProcedure/index.ts
@@ -0,0 +1,29 @@
+import * as z from 'zod';
+import { TRPCError } from '@trpc/server';
+import { integerIdSchema } from '@server/entities/shared';
+import { authenticatedProcedure } from '../authenticatedProcedure';
+import provideRepos from '../provideRepos';
+import { recipesRepository } from '../../repositories/recipesRepository';
+
+export const recipeAuthorProcedure = authenticatedProcedure
+  .use(provideRepos({ recipesRepository }))
+  .input(
+    z.object({
+      recipeId: integerIdSchema,
+    })
+  )
+  .use(async ({ input: { recipeId }, ctx: { authUser, repos }, next }) => {
+    const isAuthor = await repos.recipesRepository.isAuthor(
+      recipeId,
+      authUser.id
+    );
+
+    if (!isAuthor) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+        message: 'Recipe does not belong to the user',
+      });
+    }
+
+    return next();
+  });
---
+++ server/src/trpc/recipeAuthorProcedure/recipeAuthorProcedure.spec.ts
@@ -0,0 +1,56 @@
+import { wrapInRollbacks } from '@tests/utils/transactions';
+import { createTestDatabase } from '@tests/utils/database';
+import { insertAll } from '@tests/utils/record';
+import { fakeRecipe, fakeUser } from '@server/entities/tests/fakes';
+import { authContext } from '@tests/utils/context';
+import { createCallerFactory, router } from '..';
+import { recipeAuthorProcedure } from '.';
+
+const routes = router({
+  testCall: recipeAuthorProcedure.query(() => 'passed'),
+});
+
+const database = await wrapInRollbacks(createTestDatabase());
+
+const [userOne, userTwo] = await insertAll(database, 'users', [
+  fakeUser(),
+  fakeUser(),
+]);
+
+const [recipeOne, recipeTwo] = await insertAll(database, 'recipes', [
+  fakeRecipe({ userId: userOne.id }),
+  fakeRecipe({ userId: userTwo.id }),
+]);
+
+const createCaller = createCallerFactory(routes);
+const authenticated = createCaller(authContext({ db: database }, userOne));
+
+it('Should pass ir recipe belongs to the user', async () => {
+  const response = await authenticated.testCall({ recipeId: recipeOne.id });
+
+  expect(response).toEqual('passed');
+});
+
+it('Should throw an error if recipeId is not provided', async () => {
+  await expect((authenticated.testCall as any)({})).rejects.toThrow(/recipe/i);
+});
+
+it('Should throw an error if user provides a non-existing recipeId', async () => {
+  const nonExistantRecipeId = recipeOne.id + recipeTwo.id;
+
+  await expect(
+    authenticated.testCall({ recipeId: nonExistantRecipeId })
+  ).rejects.toThrow(/recipe/i);
+});
+
+it('Should throw an error if user provides undefined recipeId', async () => {
+  await expect(
+    (authenticated.testCall as any)({ recipeId: undefined })
+  ).rejects.toThrow(/recipe/i);
+});
+
+it('Should throw an error if recipe does not belong to the user', async () => {
+  await expect(
+    authenticated.testCall({ recipeId: recipeTwo.id })
+  ).rejects.toThrow(/recipe/i);
+});
