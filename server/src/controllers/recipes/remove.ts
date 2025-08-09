945900f7a61696c72de108c5180d8090277db900
Tomas178 <tomaspetronis77@gmail.com>
2025-08-09 19:28:31 +0300

    Remove redundancy of using repos and inputs

--- server/src/controllers/recipes/remove.ts
+++ server/src/controllers/recipes/remove.ts
@@ -1,19 +1,9 @@
-import { integerIdSchema } from '@server/entities/shared';
-import provideRepos from '@server/trpc/provideRepos';
-import { recipesRepository } from '@server/repositories/recipesRepository';
 import { TRPCError } from '@trpc/server';
 import { assertError } from '@server/utils/errors';
 import { recipeAuthorProcedure } from '@server/trpc/recipeAuthorProcedure';
-import * as z from 'zod';
 
-export default recipeAuthorProcedure
-  .use(provideRepos({ recipesRepository }))
-  .input(
-    z.object({
-      id: integerIdSchema,
-    })
-  )
-  .query(async ({ input: { id }, ctx: { repos } }) => {
+export default recipeAuthorProcedure.query(
+  async ({ input: { id }, ctx: { repos } }) => {
     try {
       const recipe = await repos.recipesRepository.remove(id);
 
@@ -28,4 +18,5 @@ export default recipeAuthorProcedure
         });
       }
     }
-  });
+  }
+);
