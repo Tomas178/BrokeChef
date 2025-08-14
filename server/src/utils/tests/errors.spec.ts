d75353e514edf673f028952d6e40f571929a5aae
Tomas178 <tomaspetronis77@gmail.com>
2025-08-14 18:52:21 +0300

    Add tests for assertPostgresError

--- server/src/utils/tests/errors.spec.ts
+++ server/src/utils/tests/errors.spec.ts
@@ -1,11 +1,26 @@
-import { assertError } from '../errors';
+import { DatabaseError } from 'pg';
+import { assertError, assertPostgresError } from '../errors';
 
-it('should not throw an error if input is an instance of Error', () => {
-  const error = new Error('Test error');
-  expect(() => assertError(error)).not.toThrow();
+describe('assertError', () => {
+  it('Should not throw an error if input is an instance of Error', () => {
+    const error = new Error('Test error');
+    expect(() => assertError(error)).not.toThrow();
+  });
+
+  it('Should throw an error if input is not an instance of Error', () => {
+    const error = 'Test error';
+    expect(() => assertError(error)).toThrow();
+  });
 });
 
-it('should throw an error if input is not an instance of Error', () => {
-  const error = 'Test error';
-  expect(() => assertError(error)).toThrow();
+describe('assertPostgresError', () => {
+  it('should not throw an error if input is an instance of Error', () => {
+    const error = new DatabaseError('Test error', 1, 'error');
+    expect(() => assertPostgresError(error)).not.toThrow();
+  });
+
+  it('should throw an error if input is not an instance of Error', () => {
+    const error = 'Test error';
+    expect(() => assertPostgresError(error)).toThrow();
+  });
 });
