318aea766ecc73c9da21fb366f3240dfe621d001
Tomas178 <tomaspetronis77@gmail.com>
2025-08-14 18:23:07 +0300

    Add assert PostgresError

--- server/src/utils/errors.ts
+++ server/src/utils/errors.ts
@@ -1,5 +1,15 @@
+import { DatabaseError } from 'pg';
+
 export function assertError(error: unknown): asserts error is Error {
   if (!(error instanceof Error)) {
+    throw error;
+  }
+}
+
+export function assertPostgresError(
+  error: unknown
+): asserts error is DatabaseError {
+  if (!(error instanceof DatabaseError)) {
     throw error;
   }
 }
