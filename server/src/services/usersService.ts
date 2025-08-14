d194198b1c20ae909dbacf10f6f28e911c3a0933
Tomas178 <tomaspetronis77@gmail.com>
2025-08-14 20:28:39 +0300

    Remove findById from service

--- server/src/services/usersService.ts
+++ server/src/services/usersService.ts
@@ -1,22 +1,11 @@
 import type { Database } from '@server/database';
 import { recipesRepository as buildRecipesRepository } from '@server/repositories/recipesRepository';
 import type { Pagination } from '@server/shared/pagination';
-import UserNotFound from '@server/utils/errors/users/UserNotFound';
-import { usersRepository as buildUsersRepository } from '../repositories/usersRepository';
 
 export function usersService(database: Database) {
-  const usersRepository = buildUsersRepository(database);
   const recipesRepository = buildRecipesRepository(database);
 
   return {
-    async findById(id: string) {
-      const user = await usersRepository.findById(id);
-
-      if (!user) throw new UserNotFound(id);
-
-      return user;
-    },
-
     async getRecipes(id: string, pagination: Pagination) {
       const [created, saved] = await Promise.all([
         recipesRepository.findCreated(id, pagination),
