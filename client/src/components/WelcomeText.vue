3d1970f8deac53e0effd450314999d5836d9975d
Tomas178 <tomaspetronis77@gmail.com>
2025-08-17 15:25:59 +0300

    Rename custom theme to more reasonable

--- client/src/components/WelcomeText.vue
+++ client/src/components/WelcomeText.vue
@@ -4,7 +4,7 @@
     src="@/assets/background-pictures/salad-bowl.webp"
   />
   <div class="mx-4 mt-10 justify-center">
-    <span class="font-primary text-random text-3xl leading-10 font-bold"
+    <span class="font-primary text-welcome-dark text-3xl leading-10 font-bold"
       >Never Worry About<br /></span
     ><span
       class="font-primary from-start to-end bg-gradient-to-b bg-clip-text text-3xl leading-10 font-bold text-transparent"
