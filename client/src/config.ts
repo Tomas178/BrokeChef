34f1fe36b4b3229d2a333ac3a8def557bc855725
Tomas178 <tomaspetronis77@gmail.com>
2025-08-19 11:35:59 +0300

    Add config for reset password path

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,18 +1,25 @@
-const DEFAULT_FRONTEND_ORIGIN = 'http://localhost:5173';
-
 export const apiOrigin =
   (import.meta.env.VITE_API_ORIGIN as string) || window.location.origin;
 export const apiPath =
   (import.meta.env.VITE_API_PATH as string) || '/api/v1/trpc';
 export const apiBase = `${apiOrigin}${apiPath}`;
 
-export const frontendBase =
-  (import.meta.env.VITE_BASE_ORIGIN as string) || DEFAULT_FRONTEND_ORIGIN;
-
-if (typeof apiOrigin !== 'string') {
+if (typeof apiOrigin !== 'string')
   throw new Error('VITE_API_ORIGIN is not defined');
-}
 
-if (typeof apiPath !== 'string') {
+if (typeof apiPath !== 'string')
   throw new Error('VITE_API_PATH is not defined');
-}
+
+export const frontendBase =
+  (import.meta.env.VITE_BASE_ORIGIN as string) || 'http://localhost:5173';
+
+export const resetPasswordPath =
+  (import.meta.env.VITE_RESET_PASSWORD_PATH as string) || '/reset-password';
+
+export const resetPasswordBase = `${frontendBase}${resetPasswordPath}`;
+
+if (typeof frontendBase !== 'string')
+  throw new Error('VITE_BASE_ORIGIN is not defined');
+
+if (typeof resetPasswordPath !== 'string')
+  throw new Error('VITE_RESET_PASSWORD_PATH is not defined');
