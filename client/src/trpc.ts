e8bcb5879022c28af9355db7b33f4ea5e943183d
Tomas178 <tomaspetronis77@gmail.com>
2025-08-19 15:05:37 +0300

    import superjson not { SuperJSON }

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -1,13 +1,13 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '@server/shared/trpc';
-import { SuperJSON } from 'superjson';
+import superjson from 'superjson';
 import { apiBase } from './config';
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: apiBase,
-      transformer: SuperJSON,
+      transformer: superjson,
       fetch(url, options) {
         return fetch(url, {
           ...options,
