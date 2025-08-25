1d6f67e615be802f6560102a5c6700ef37fc4d49
Tomas178 <tomaspetronis77@gmail.com>
2025-08-26 00:55:56 +0300

    Fix a bug where on first load of page only header is displayed

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -18,7 +18,7 @@ const router = createRouter({
       component: MainLayout,
       children: [
         {
-          path: '/',
+          path: '',
           name: 'Home',
           component: HomeView,
         },
