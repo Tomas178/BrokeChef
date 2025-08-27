812fbe8e8e61104196542e06dae568147b119aa6
Tomas178 <tomaspetronis77@gmail.com>
2025-08-27 19:40:50 +0300

    Add basic design and functionality of saving unsaving

--- client/src/views/RecipeView.vue
+++ client/src/views/RecipeView.vue
@@ -5,14 +5,62 @@ import type { RecipesPublic } from '@server/shared/types';
 import { onBeforeMount, ref } from 'vue';
 import { titleCase } from 'title-case';
 import { format } from 'date-fns';
+import useErrorMessage from '@/composables/useErrorMessage';
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+import { DEFAULT_SERVER_ERROR } from '@/consts';
 
 const route = useRoute();
+
 const recipe = ref<RecipesPublic>();
+const isAuthor = ref(false);
+const isSaved = ref(false);
 
 const recipeId = Number(route.params.id);
 
 onBeforeMount(async () => {
   recipe.value = await trpc.recipes.findById.query(recipeId);
+  isAuthor.value = await trpc.recipes.isAuthor.query(recipeId);
+  isSaved.value = await trpc.savedRecipes.isSaved.query(recipeId);
+  console.log(recipe.value);
+  console.log(isAuthor.value);
+  console.log(isSaved.value);
+});
+
+const [saveRecipe] = useErrorMessage(async () => {
+  await toast.promise(trpc.savedRecipes.save.mutate(recipeId), {
+    pending: 'Saving recipe...',
+    success: {
+      render() {
+        isSaved.value = true;
+        return 'Recipe saved successfully';
+      },
+    },
+    error: {
+      render(err) {
+        if (err?.data?.message) return err.data.message;
+        return DEFAULT_SERVER_ERROR;
+      },
+    },
+  });
+});
+
+const [unsaveRecipe] = useErrorMessage(async () => {
+  await toast.promise(trpc.savedRecipes.unsave.mutate(recipeId), {
+    pending: 'Unsaving recipe...',
+    success: {
+      render() {
+        isSaved.value = false;
+        return 'Recipe unsaved successfully';
+      },
+    },
+    error: {
+      render(err) {
+        if (err?.data?.message) return err.data.message;
+        return DEFAULT_SERVER_ERROR;
+      },
+    },
+  });
 });
 </script>
 
@@ -29,10 +77,27 @@ onBeforeMount(async () => {
             </div>
             <div class="flex justify-start">
               <button
+                v-if="isAuthor"
                 type="button"
                 class="rounded-3xl bg-red-400 px-6 py-2 text-xl leading-tight font-medium text-white hover:scale-105"
               >
                 Delete
+              </button>
+              <button
+                v-else-if="!isSaved"
+                @click="saveRecipe"
+                type="button"
+                class="gradient-action-button rounded-3xl px-6 py-2 text-xl leading-tight font-medium text-white hover:scale-105"
+              >
+                Save
+              </button>
+              <button
+                v-else
+                @click="unsaveRecipe"
+                type="button"
+                class="gradient-action-button rounded-3xl px-6 py-2 text-xl leading-tight font-medium text-white hover:scale-105"
+              >
+                Unsave
               </button>
             </div>
           </div>
