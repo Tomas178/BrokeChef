8ccc1ecb15d0f5f8fd1b0abccaba97c6992df395
Tomas178 <tomaspetronis77@gmail.com>
2025-08-28 15:57:04 +0300

    findById returns recipes with author, ingredients and tools

--- server/src/controllers/recipes/tests/recipesFindById.spec.ts
+++ server/src/controllers/recipes/tests/recipesFindById.spec.ts
@@ -2,7 +2,13 @@ import { createCallerFactory } from '@server/trpc';
 import { wrapInRollbacks } from '@tests/utils/transactions';
 import { createTestDatabase } from '@tests/utils/database';
 import { insertAll } from '@tests/utils/record';
-import { fakeRecipe, fakeUser } from '@server/entities/tests/fakes';
+import {
+  fakeIngredient,
+  fakeRecipe,
+  fakeTool,
+  fakeUser,
+} from '@server/entities/tests/fakes';
+import { joinStepsToArray } from '@server/repositories/utils/joinStepsToArray';
 import recipesRouter from '..';
 
 const createCaller = createCallerFactory(recipesRouter);
@@ -13,21 +19,59 @@ const [user, userOther] = await insertAll(database, 'users', [
   fakeUser(),
 ]);
 
-const [recipe, recipeOther] = await insertAll(database, 'recipes', [
+const [recipeOne, recipeTwo] = await insertAll(database, 'recipes', [
   fakeRecipe({ userId: user.id }),
   fakeRecipe({ userId: userOther.id }),
+]);
+
+const [ingredientOne, ingredientTwo] = await insertAll(
+  database,
+  'ingredients',
+  [fakeIngredient(), fakeIngredient()]
+);
+
+const [toolOne, toolTwo] = await insertAll(database, 'tools', [
+  fakeTool(),
+  fakeTool(),
+]);
+
+await insertAll(database, 'recipesIngredients', [
+  {
+    recipeId: recipeOne.id,
+    ingredientId: ingredientOne.id,
+  },
+  {
+    recipeId: recipeOne.id,
+    ingredientId: ingredientTwo.id,
+  },
+]);
+
+await insertAll(database, 'recipesTools', [
+  {
+    recipeId: recipeOne.id,
+    toolId: toolOne.id,
+  },
+  {
+    recipeId: recipeOne.id,
+    toolId: toolTwo.id,
+  },
 ]);
 
 const { findById } = createCaller({ db: database });
 
 it('Should return a recipe', async () => {
-  const recipeResponse = await findById(recipe.id);
+  const recipeResponse = await findById(recipeOne.id);
 
-  expect(recipeResponse).toMatchObject(recipe);
+  expect(recipeResponse).toMatchObject({
+    ...recipeOne,
+    steps: joinStepsToArray(recipeOne.steps),
+    ingredients: [ingredientOne.name, ingredientTwo.name],
+    tools: [toolOne.name, toolTwo.name],
+  });
 });
 
 it('Should throw an error if the recipe does not exist', async () => {
-  const nonExistantId = recipe.id + recipeOther.id;
+  const nonExistantId = recipeOne.id + recipeTwo.id;
 
   await expect(findById(nonExistantId)).rejects.toThrow(/not found/i);
 });
