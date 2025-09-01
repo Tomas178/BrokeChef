e6af6f032b84a8d4356636e1fdaa10dd0d177a8c
Tomas178 <tomaspetronis77@gmail.com>
2025-09-01 10:45:21 +0300

    Remove route needed to testing better-auth

--- server/src/controllers/auth.ts
+++
@@ -1,8 +0,0 @@
-import { router } from '@server/trpc';
-import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure';
-
-export const authRouter = router({
-  me: authenticatedProcedure.query(({ ctx }) => {
-    return ctx.authUser;
-  }),
-});
--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,11 +1,9 @@
 import { router } from '../trpc';
-import { authRouter } from './auth';
 import recipes from './recipes';
 import savedRecipes from './savedRecipes';
 import users from './users';
 
 export const appRouter = router({
-  auth: authRouter, // Temporary for testing better-auth
   recipes,
   savedRecipes,
   users,
