ad6a2e24509476dfbafb095262d8b0ffdeec32cc
Tomas178 <tomaspetronis77@gmail.com>
2025-09-03 00:59:44 +0300

    Test more readble

--- server/src/utils/AWSS3Client/tests/signGetUrl.spec.ts
+++ server/src/utils/AWSS3Client/tests/signGetUrl.spec.ts
@@ -1,8 +1,10 @@
 import type { S3Client } from '@aws-sdk/client-s3';
 import { signGetUrl } from '../signGetUrl';
 
+const fakeSignedUrl = 'https://signed-url.com/folder/image.png';
+
 vi.mock('@aws-sdk/s3-request-presigner', () => ({
-  getSignedUrl: vi.fn(() => 'https://signed-url.com/folder/image.png'),
+  getSignedUrl: vi.fn(() => fakeSignedUrl),
 }));
 
 const mockS3Client = {} as S3Client;
@@ -12,6 +14,6 @@ describe('signGetUrl', () => {
   it('Should sign url and return it', async () => {
     const signedUrl = await signGetUrl(mockS3Client, fakeKey);
 
-    expect(signedUrl).toBe('https://signed-url.com/folder/image.png');
+    expect(signedUrl).toBe(fakeSignedUrl);
   });
 });
