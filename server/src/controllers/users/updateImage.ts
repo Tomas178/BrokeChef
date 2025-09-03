e98aa2898c6590644f893c46cb301f8c6771e735
Tomas178 <tomaspetronis77@gmail.com>
2025-09-03 03:10:53 +0300

    Use users service

--- server/src/controllers/users/updateImage.ts
+++ server/src/controllers/users/updateImage.ts
@@ -1,17 +1,17 @@
 import { authenticatedProcedure } from '@server/trpc/authenticatedProcedure';
-import provideRepos from '@server/trpc/provideRepos';
-import { usersRepository } from '@server/repositories/usersRepository';
 import * as z from 'zod';
 import UserNotFound from '@server/utils/errors/users/UserNotFound';
 import { TRPCError } from '@trpc/server';
 import { signImages } from '@server/utils/signImages';
+import provideServices from '@server/trpc/provideServices';
+import { usersService } from '@server/services/usersService';
 
 export default authenticatedProcedure
-  .use(provideRepos({ usersRepository }))
+  .use(provideServices({ usersService }))
   .input(z.string().nonempty())
-  .mutation(async ({ input: imageUrl, ctx: { authUser, repos } }) => {
+  .mutation(async ({ input: imageUrl, ctx: { authUser, services } }) => {
     try {
-      let updated = await repos.usersRepository.updateImage(
+      let updated = await services.usersService.updateImage(
         authUser.id,
         imageUrl
       );
