c66a94b0f241439a1dc0724c6938b18c32bed5b7
Tomas178 <tomaspetronis77@gmail.com>
2025-09-05 16:53:41 +0300

    Add a method to get totalCreated recipes by user

--- server/src/controllers/users/index.ts
+++ server/src/controllers/users/index.ts
@@ -4,11 +4,13 @@ import findById from './findById';
 import updateImage from './updateImage';
 import getCreatedRecipes from './getCreatedRecipes';
 import getSavedRecipes from './getSavedRecipes';
+import totalCreated from './totalCreated';
 
 export default router({
   getRecipes,
   getCreatedRecipes,
   getSavedRecipes,
+  totalCreated,
   findById,
   updateImage,
 });
---
+++ server/src/controllers/users/tests/usersTotalCreated.spec.ts
@@ -0,0 +1,33 @@
+import { createCallerFactory } from '@server/trpc';
+import { wrapInRollbacks } from '@tests/utils/transactions';
+import { createTestDatabase } from '@tests/utils/database';
+import { insertAll } from '@tests/utils/record';
+import { fakeRecipe, fakeUser } from '@server/entities/tests/fakes';
+import { authContext, requestContext } from '@tests/utils/context';
+import usersRouter from '..';
+
+const createCaller = createCallerFactory(usersRouter);
+const database = await wrapInRollbacks(createTestDatabase());
+
+const [user] = await insertAll(database, 'users', fakeUser());
+
+const { totalCreated } = createCaller(authContext({ db: database }, user));
+
+it('Should throw an error if user is not authenticated', async () => {
+  const { totalCreated } = createCaller(requestContext({ db: database }));
+
+  await expect(totalCreated()).rejects.toThrow(/unauthenticated/i);
+});
+
+it('Should return 0', async () => {
+  await expect(totalCreated()).resolves.toBe(0);
+});
+
+it('Should return the same number that was created', async () => {
+  const created = await insertAll(database, 'recipes', [
+    fakeRecipe({ userId: user.id }),
+    fakeRecipe({ userId: user.id }),
+  ]);
+
+  await expect(totalCreated()).resolves.toBe(created.length);
+});
