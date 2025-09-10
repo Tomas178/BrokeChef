966b3f802214b4ac6ea82f39960a2f10e7d684f6
Tomas178 <tomaspetronis77@gmail.com>
2025-09-10 14:52:54 +0300

    Add basic tests for signup/login

--- client/e2e/user.test.ts
+++ client/e2e/user.test.ts
@@ -1,24 +1,118 @@
 import { test, expect } from '@playwright/test';
+import { fakeUser } from './utils/fakeData';
+
+const user = fakeUser();
 
 test.describe.serial('Signup and login sequence', () => {
-  test('Visitor can signup', async ({ page }) => {
+  const URL_LOGGED_IN = '/';
+
+  test('Visitor is shown that passwords do not match', async ({ page }) => {
     await page.goto('/signup');
     const toastContainer = page.getByTestId('toast-body');
+    const errorMessage = page.getByTestId('errorMessage');
 
-    await expect(toastContainer).toHaveCount(0);
+    await expect(toastContainer).toBeHidden();
+    await expect(errorMessage).toBeHidden();
 
     const form = page.getByRole('form', { name: 'Signup' });
 
-    await form.locator('input[data-testid="username"]').fill('username123');
-    await form.locator('input[data-testid="email"]').fill('username@gmail.com');
-    await form.locator('input[data-testid="password"]').fill('password123');
+    await form.locator('input[data-testid="username"]').fill(user.name);
+    await form.locator('input[data-testid="email"]').fill(user.email);
+    await form.locator('input[data-testid="password"]').fill(user.password);
     await form
       .locator('input[data-testid="repeat-password"]')
-      .fill('password123');
+      .fill(user.password + 'a');
 
     await form.locator('button[type="submit"]').click();
 
     await expect(toastContainer).toBeVisible();
-    // await expect(toastContainer).toHaveText(/creating/i);
+    await expect(toastContainer).toHaveText(/passwords/i);
+
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toHaveText(/passwords/i);
+  });
+
+  test('Visitor can signup', async ({ page }) => {
+    await page.goto('/signup');
+    const toastContainer = page.getByTestId('toast-body');
+
+    await expect(toastContainer).toBeHidden();
+
+    const form = page.getByRole('form', { name: 'Signup' });
+
+    await form.locator('input[data-testid="username"]').fill(user.name);
+    await form.locator('input[data-testid="email"]').fill(user.email);
+    await form.locator('input[data-testid="password"]').fill(user.password);
+    await form
+      .locator('input[data-testid="repeat-password"]')
+      .fill(user.password);
+
+    await form.locator('button[type="submit"]').click();
+
+    await expect(toastContainer).toBeVisible();
+    await expect(toastContainer).toHaveText(/creating/i);
+  });
+
+  test.describe('Redirections based on login status', () => {
+    test('Visitor can access homepage without being logged in', async ({
+      page,
+    }) => {
+      await page.goto('/');
+
+      await page.waitForURL('/');
+
+      await expect(page).toHaveURL('/');
+    });
+
+    test('Visitor cannot access profile page without being logged in', async ({
+      page,
+    }) => {
+      await page.goto('/profile');
+
+      await page.waitForURL('/login');
+
+      await expect(page).toHaveURL('/login');
+    });
+
+    test('Visitor cannot access recipe page without being logged in', async ({
+      page,
+    }) => {
+      await page.goto('/recipe/1');
+
+      await page.waitForURL('/login');
+
+      await expect(page).toHaveURL('/login');
+    });
+
+    test('Visitor cannot access create recipe page without being logged in', async ({
+      page,
+    }) => {
+      await page.goto('/create-recipe');
+
+      await page.waitForURL('/login');
+
+      await expect(page).toHaveURL('/login');
+    });
+  });
+
+  test('Visitor is shown that email needs to be verified', async ({ page }) => {
+    await page.goto('/login');
+    const toastContainer = page.getByTestId('toast-body');
+
+    await expect(toastContainer).toBeHidden();
+
+    const form = page.getByRole('form', { name: 'Signin' });
+    await form.locator('input[data-testid="email"]').fill(user.email);
+    await form.locator('input[data-testid="password"]').fill(user.password);
+
+    await form.locator('button[type="submit"]').click();
+
+    await expect(page).toHaveURL('/login');
+
+    await expect(toastContainer).toBeVisible();
+    await expect(toastContainer).toHaveText(/email/i);
+
+    await page.reload();
+    await expect(page).toHaveURL('/login');
   });
 });
--- client/e2e/utils/fakeData.ts
+++ client/e2e/utils/fakeData.ts
@@ -4,12 +4,11 @@ import { Chance } from 'chance';
 
 export const random = process.env.CI ? Chance(1) : Chance();
 
-export const fakeSignupUser = <T extends Insertable<Users>>(
+export const fakeUser = <T extends Insertable<Users>>(
   overrides: Partial<T> = {} as T
 ) => ({
   name: random.string(),
   email: random.email(),
   password: 'password.123',
-  image: undefined,
   ...overrides,
 });
