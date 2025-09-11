2618d4f0dc4e366b181fdb31e5d985c867fc477a
Tomas178 <tomaspetronis77@gmail.com>
2025-09-11 14:28:17 +0300

    Extract latest emaail link for verifying or resetting password

--- client/e2e/utils/mailhog.ts
+++ client/e2e/utils/mailhog.ts
@@ -1,7 +1,7 @@
 const BASE_URL_V1 = 'http://localhost:8025/api/v1';
 const BASE_URL_V2 = 'http://localhost:8025/api/v2';
 
-export async function getVerifyLink(): Promise<string> {
+export async function getLatestEmailLink(): Promise<string> {
   const response = await fetch(`${BASE_URL_V2}/messages`, { method: 'GET' });
 
   const data = await response.json();
@@ -10,12 +10,14 @@ export async function getVerifyLink(): Promise<string> {
   const htmlCleared = html.replace(/=\r\n/g, '').replace(/=3D/g, '=');
 
   const match = htmlCleared.match(
-    /http:\/\/localhost:3000\/api\/auth\/verify-email\?token=[\w.*-]+&callbackURL=http:\/\/localhost:5173/
+    /(http:\/\/localhost:3000\/api\/auth\/verify-email\?token=[\w.-]+&callbackURL=http:\/\/localhost:5173|http:\/\/localhost:3000\/api\/auth\/reset-password\/[^\s"]+)/
   );
 
   if (!match) {
-    throw new Error('Verification link not found in email body');
+    throw new Error('Link not found in email body');
   }
+
+  console.log(match[0]);
 
   return match[0];
 }
