034ef9050260925167e43c9f0c1bc17750192599
Tomas178 <tomaspetronis77@gmail.com>
2025-09-11 10:58:05 +0300

    Remove unneccessary functions

--- client/e2e/utils/api.ts
+++ client/e2e/utils/api.ts
@@ -3,10 +3,6 @@ import type { AppRouter } from '@server/shared/trpc';
 import superjson from 'superjson';
 import { apiOrigin, apiPath } from './config';
 import { createAuthClient } from 'better-auth/vue';
-import { fakeUser } from './fakeData';
-import { Page } from '@playwright/test';
-
-let accessToken: string | null = null;
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
@@ -32,61 +28,3 @@ export const authClient = createAuthClient({
 
 export type userSignup = Parameters<typeof authClient.signUp.email>[0];
 export type UserLogin = Parameters<typeof authClient.signIn.email>[0];
-type UserLoginAuthed = UserLogin & { id: string; accessToken: string };
-
-export async function loginNewUser(
-  userLogin: UserLogin = fakeUser()
-): Promise<UserLoginAuthed> {
-  try {
-    await authClient.signIn.email(userLogin);
-  } catch (error) {
-    throw error;
-  }
-
-  const { data: loginResponse } = await authClient.signIn.email(userLogin);
-
-  if (!loginResponse) {
-    throw new Error('Login failed: no response from authClient');
-  }
-
-  const userId = loginResponse.user.id;
-
-  return {
-    ...userLogin,
-    id: userId,
-    accessToken: loginResponse.token,
-  };
-}
-
-export async function asUser<T>(
-  page: Page,
-  userLogin: UserLogin,
-  callback: (user: UserLoginAuthed) => Promise<T>
-): Promise<T> {
-  const [user] = await Promise.all([
-    loginNewUser(userLogin),
-    (async () => {
-      if (page.url() === 'about:blank') {
-        await page.goto('/');
-        await page.waitForURL('/');
-      }
-    })(),
-  ]);
-
-  accessToken = user.accessToken;
-  await page.evaluate(
-    ({ accessToken }) => {
-      localStorage.setItem('authToken', accessToken);
-    },
-    { accessToken }
-  );
-
-  const callbackResult = await callback(user);
-
-  await page.evaluate(() => {
-    localStorage.removeItem('authToken');
-  });
-  accessToken = null;
-
-  return callbackResult;
-}
