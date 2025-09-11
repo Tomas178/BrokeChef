e32a1354b814177d4132cfa1e136a04ad4335c30
Tomas178 <tomaspetronis77@gmail.com>
2025-09-11 15:44:34 +0300

    Ad utils for notifications

---
+++ client/e2e/utils/toast.ts
@@ -0,0 +1,20 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export async function getToastContainer(page: Page): Promise<Locator> {
+  const toastContainer = page.getByTestId('toast-body');
+  await expect(toastContainer).toBeHidden();
+
+  return toastContainer;
+}
+
+export async function getErrorMessage(page: Page): Promise<Locator> {
+  const errorMessage = page.getByTestId('error-message');
+  await expect(errorMessage).toBeHidden();
+
+  return errorMessage;
+}
+
+export async function checkLocator(locator: Locator, message: string | RegExp) {
+  await expect(locator).toBeVisible();
+  await expect(locator).toHaveText(message);
+}
