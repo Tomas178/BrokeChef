e20f0e3b9042cc92cd035711750c28cc708e9b80
Tomas178 <tomaspetronis77@gmail.com>
2025-09-27 22:24:47 +0300

    Remove unneccesarry indexes

--- server/src/database/migrations/20250814T095132Z-addRecipesIndexes.ts
+++ server/src/database/migrations/20250814T095132Z-addRecipesIndexes.ts
@@ -13,48 +13,6 @@ export async function up(database: Kysely<any>) {
     .on('saved_recipes')
     .columns(['user_id', 'recipe_id'])
     .execute();
-
-  await database.schema
-    .createIndex('idx_users_id')
-    .on('users')
-    .column('id')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_ingredients_id')
-    .on('ingredients')
-    .column('id')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_ingredients_name')
-    .on('ingredients')
-    .column('name')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_recipes_ingredients_recipe_id')
-    .on('recipes_ingredients')
-    .column('recipe_id')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_tools_id')
-    .on('tools')
-    .column('id')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_tools_name')
-    .on('tools')
-    .column('name')
-    .execute();
-
-  await database.schema
-    .createIndex('idx_recipes_tools_recipe_id')
-    .on('recipes_tools')
-    .column('recipe_id')
-    .execute();
 }
 
 export async function down(database: Kysely<any>) {
@@ -66,19 +24,9 @@ export async function down(database: Kysely<any>) {
     .dropIndex('idx_saved_recipes_user_id_recipe_id')
     .execute();
 
-  await database.schema.dropIndex('idx_users_id').execute();
-
-  await database.schema.dropIndex('idx_ingredients_id').execute();
-
-  await database.schema.dropIndex('idx_ingredients_name').execute();
-
   await database.schema
     .dropIndex('idx_recipes_ingredients_recipe_id')
     .execute();
-
-  await database.schema.dropIndex('idx_tools_id').execute();
-
-  await database.schema.dropIndex('idx_tools_name').execute();
 
   await database.schema.dropIndex('idx_recipes_tools_recipe_id').execute();
 }
