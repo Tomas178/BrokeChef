c8037a38a3d044c5c238491e982ae3860ba17eed
Tomas178 <tomaspetronis77@gmail.com>
2025-09-30 23:00:53 +0300

    Include ratings service to provide services

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -2,10 +2,16 @@ import type { Database } from '@server/database';
 import { recipesService } from './recipesService';
 import { savedRecipesService } from './savedRecipesService';
 import { usersService } from './usersService';
+import { ratingsService } from './ratingsService';
 
 export type ServiceFactory = <T>(database: Database) => T;
 
-const services = { recipesService, savedRecipesService, usersService };
+const services = {
+  recipesService,
+  savedRecipesService,
+  usersService,
+  ratingsService,
+};
 
 export type ServicesFactories = typeof services;
 export type Services = {
