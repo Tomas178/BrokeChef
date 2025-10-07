4bd913d03312e9c2c19b73891d4447a4d4499d2f
Tomas178 <tomaspetronis77@gmail.com>
2025-10-07 19:53:31 +0300

    Add enum for route names

---
+++ client/src/router/routeNames.ts
@@ -0,0 +1,12 @@
+export enum RouteNames {
+  HOME = 'Home',
+  CREATE_RECIPES = 'CreateRecipe',
+  MY_PROFILE = 'MyProfile',
+  USER_PROFILE = 'UserProfile',
+  RECIPE = 'Recipe',
+  EDIT_MY_PROFILE = 'EditMyProfile',
+  SIGNUP = 'Signup',
+  LOGIN = 'Login',
+  RESET_PASSWORD = 'ResetPassword',
+  REQUEST_RESET_PASSWORD = 'RequestResetPassword',
+}
