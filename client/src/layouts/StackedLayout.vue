1e5be93a19220e0dd53762de57059ce111e66e34
Tomas178 <tomaspetronis77@gmail.com>
2025-12-08 19:29:36 +0200

    Add light/dark mode toggle button

--- client/src/layouts/StackedLayout.vue
+++ client/src/layouts/StackedLayout.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { FwbNavbar, FwbNavbarCollapse, FwbNavbarLink } from 'flowbite-vue';
 import { ROUTE_NAMES, type RouteNamesValues } from '@/router/consts/routeNames';
+import { onMounted } from 'vue';
 
 const { links } = defineProps<{
   links: {
@@ -19,6 +20,31 @@ const navigation = computed(() =>
     isActive: route.name === item.name,
   }))
 );
+
+const isDarkMode = ref(false);
+
+watch(isDarkMode, (newVal) => {
+  if (newVal) {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+onMounted(() => {
+  const savedTheme = localStorage.getItem('theme');
+  const systemPrefersDark = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+
+  if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
+    isDarkMode.value = true;
+  } else {
+    isDarkMode.value = false;
+  }
+});
 </script>
 
 <template>
@@ -52,7 +78,24 @@ const navigation = computed(() =>
         >
           {{ link.label }}
         </FwbNavbarLink>
+
         <slot name="menu" />
+
+        <button
+          type="button"
+          @click="isDarkMode = !isDarkMode"
+          class="hover:text-secondary-green ml-4 flex cursor-pointer items-center justify-center rounded-lg px-3 py-2 text-gray-500 dark:bg-inherit dark:text-gray-400 dark:hover:text-white"
+        >
+          <span
+            v-if="isDarkMode"
+            class="material-symbols-outlined text-yellow-300"
+          >
+            light_mode
+          </span>
+          <span v-else class="material-symbols-outlined text-gray-600">
+            dark_mode
+          </span>
+        </button>
       </FwbNavbarCollapse>
     </template>
   </FwbNavbar>
@@ -63,8 +106,12 @@ const navigation = computed(() =>
 <style scoped>
 @reference "../assets/index.css";
 
+:deep(ul) {
+  @apply items-center;
+}
+
 :deep(ul > a) {
-  @apply bg-background-form-light dark:bg-inherit;
+  @apply bg-background-form-light flex items-center justify-center px-3 py-2 dark:bg-inherit;
 }
 
 :deep(ul .router-link-exact-active) {
