a85b3692a173296cac09739c931ade4995dc0a96
Tomas178 <tomaspetronis77@gmail.com>
2026-01-14 20:52:05 +0200

    Fix error checking for e2e tests

--- client/src/utils/assertValidFile.ts
+++ client/src/utils/assertValidFile.ts
@@ -3,7 +3,7 @@ import { MAX_FILE_SIZE } from '@server/shared/consts';
 
 export function assertValidFile(file: File) {
   if (!allowedMimetypesArray.includes(file.type)) {
-    throw new Error('Wron Image Type');
+    throw new Error('Supported types for image are .png, .jpg or .jpeg');
   }
 
   if (file.size > MAX_FILE_SIZE) {
