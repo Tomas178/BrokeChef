1019f4f652f326063047301fbe89c4a60d349de6
Tomas178 <tomaspetronis77@gmail.com>
2026-02-08 17:38:52 +0200

    New OpenAPI generating

--- server/scripts/generateOpenApiDocument.ts
+++ server/scripts/generateOpenApiDocument.ts
@@ -4,6 +4,9 @@ import { fileURLToPath } from 'node:url';
 import { appRouter } from '@server/controllers';
 import { generateOpenApiDocument } from 'trpc-to-openapi';
 
+const OPENAPI_FOLDER_NAME = 'openapi';
+const FILENAME = 'openapi-spec.json';
+
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: 'BrokeChef API',
   description: 'OpenAPI Documentation for the BrokeChef tRPC API',
@@ -11,14 +14,18 @@ export const openApiDocument = generateOpenApiDocument(appRouter, {
   baseUrl: 'http://localhost:3000/api/v1',
 });
 
-const outputPath = path.resolve(
+const OUTPUT_PATH = path.resolve(
   fileURLToPath(import.meta.url),
-  '../openapi-spec.json'
+  `../../${OPENAPI_FOLDER_NAME}/${FILENAME}`
 );
 
 try {
-  fs.writeFileSync(outputPath, JSON.stringify(openApiDocument, undefined, 2));
-  console.log('OpenAPI document generated successfully at:', outputPath);
+  const OUTPUT_DIRECTORY = path.dirname(OUTPUT_PATH);
+
+  fs.mkdirSync(OUTPUT_DIRECTORY, { recursive: true });
+
+  fs.writeFileSync(OUTPUT_PATH, JSON.stringify(openApiDocument, undefined, 2));
+  console.log('OpenAPI document generated successfully at:', OUTPUT_PATH);
 } catch (error) {
   console.error('Failed to generate OpenAPI document:', error);
 }
